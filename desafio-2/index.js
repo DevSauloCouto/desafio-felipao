afe8434f7204ea1ac8c4a61024654ddb00710574
SauloCouto <143052328+DevSauloCouto@users.noreply.github.com>
2023-10-28 15:48:00 -0300

    Atualizado estrutura condicional

--- desafio-2/index.js
+++ desafio-2/index.js
@@ -1,27 +1,13 @@
 function rank(win, lose) {
     const wins = win - lose;
     let levelRank = function(){
-        if(wins <= 10) {
-            return "Ferro";
-        }
-        if(wins > 10 && wins < 21) {
-            return "Bronze";
-        }
-        if(wins > 20 && wins < 51) {
-            return "Prata";
-        }
-        if(wins > 50 && wins < 81) {
-            return "Ouro";
-        }
-        if(wins > 80 && wins < 91) {
-            return "Diamante";
-        }
-        if(wins > 90 && wins < 101) {
-            return "Lendário";
-        }
-        if(wins > 100) {
-            return "Imortal";
-        }
+        if(wins <= 10) return "Ferro";
+        if(wins > 10 && wins < 21) return "Bronze";
+        if(wins > 20 && wins < 51) return "Prata";
+        if(wins > 50 && wins < 81) return "Ouro";
+        if(wins > 80 && wins < 91) return "Diamante";
+        if(wins > 90 && wins < 101) return "Lendário";
+        if(wins > 100) return "Imortal";
     }
     const msg = `O Herói tem um saldo de ${wins} e está no nível ${levelRank()}`;
     return msg;
